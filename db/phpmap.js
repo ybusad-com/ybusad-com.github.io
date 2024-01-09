78dfd5b2373c945e585e8a5b54d97af0cd67b77f
Your Name <you@example.com>
2024-01-09 16:08:59 +0800

    README.md

--- db/phpmap.js
+++ db/phpmap.js
@@ -3,7 +3,7 @@
 !function(){
   var maps = {
     '/v2/article/comment/getList.php': '/db/article/comment/<id>.json',
-    '/v2/article/index/getList.php': '/db/article/page/<pageNumber>.json',
+    '/v2/article/index/getList.php': '/db/article/page<pageNumber>.json',
     '/v2/article/item/getDetail.php': '/db/article/article_<urlCode>.json',
   };
   var open = XMLHttpRequest.prototype.open;
@@ -16,7 +16,7 @@
         var x = v.indexOf('=');
         arg[v.slice(0,x)] = v.slice(x+1);
       });
-      console.log (url, arg);
+      // console.log (url, arg);
       url = url.slice(0,spl);
       try {
         arguments[1] = maps[url].replace(/<(.*?)>/g,
